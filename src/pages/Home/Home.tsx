224f531dab33319175f85588d80364ae63421eeb
jvs4nt <joaovit342@gmail.com>
2024-04-01 17:02:12 -0300

    ajuste

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -3,8 +3,14 @@ import Navbar from '../../components/Navbar/Navbar';
 import { Box1, Box2, Button, ButtonDiv, Card, CardBox, CardImage, CardLink, CardText, Img, MainBox, MainSection, SecButton, SecondaryBox } from './Home.style';
 import card1 from './../../assets/card1.png';
 import card3 from './../../assets/card3.png';
+import { useNavigate } from 'react-router-dom';
 
 const Home: React.FC = () => {
+    const navigate = useNavigate();
+
+    function handleTeam() {
+        navigate('/team');
+    }
 
     return (
         <>
@@ -31,21 +37,21 @@ const Home: React.FC = () => {
                     <CardImage src={card1}/>
                     <CardBox>
                         <CardText>Temos diversos produtos disponíveis para atender suas necessidades</CardText>
-                        <CardLink>Conheça os produtos</CardLink>
+                        <CardLink href='https://www.salesforce.com/br/products/' target='_blank'>Conheça os produtos</CardLink>
                     </CardBox>
                 </Card>
                 <Card>
                     <CardImage src='https://i.ibb.co/Y0KWH0n/div-card-image-wrapper.png'/>
                     <CardBox>
                         <CardText>Nossa equipe é determinada e mantemos o foco na exatidão e garantia</CardText>
-                        <CardLink>Conheça a equipe</CardLink>
+                        <CardLink onClick={handleTeam}>Conheça a equipe</CardLink>
                     </CardBox>
                 </Card>
                 <Card>
                     <CardImage src={card3}/>
                     <CardBox>
                         <CardText>Tiramos todas as suas dúvidas através do nosso contato 24h</CardText>
-                        <CardLink>Entre em contato conosco</CardLink>
+                        <CardLink href='https://www.salesforce.com/br/form/contact/contactme/?d=cta-header-9' target='_blank'>Entre em contato conosco</CardLink>
                     </CardBox>
                 </Card>
             </SecondaryBox>
